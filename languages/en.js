9f029e0dbf880e6640af71e572899a3a1e98a6b5
ZhuoranLyu <zl953@nyu.edu>
2015-07-08 15:55:35 -0400

    add rules to the game

--- languages/en.js
+++ languages/en.js
@@ -1,9 +1,14 @@
 window.angularTranslationsLoaded('en',{
 "RULES_OF_CHEAT":"Rules of Cheat",
-"RULES_SLIDE1":"Cheat (also known as Liar, Bullshit and I Doubt It) is a card game where the players aim to get rid of all of their cards.",
-"RULES_SLIDE2":"You and your opponent take turns to make claim. Drag the cards to the middle part to select one or more cards.",
-"RULES_SLIDE3":"You can select at most 4 cards to make a claim, to make a claim, please click on the 'Make a claim' button. Then you can select which rank you wanna claim.",
-"RULES_SLIDE4":"You can call cheater or just pass when it's your turn.",
+"RULES_SLIDE1":"Cheat (also known as Liar, Bullshit and I Doubt It) is a card game where the players \
+				aim to get rid of all of their cards.",
+"RULES_SLIDE2":"You and your opponent take turns to make claim. Drag the cards to the middle part to select \
+				one or more cards.",
+"RULES_SLIDE3":"You can select at most 4 cards to make a claim, to make a claim, please click on the \
+				'Make a claim' button. Then you can select which rank you wanna claim.",
+"RULES_SLIDE4":"You can call cheater or just pass when it's your turn. If the another player wasn't cheating, \
+				the person who called them out has to add the cards in the middle to his or her hand.\
+				If the first player gets caught in his or her lie, he or she has to add the cards back into their hand.",
 "RULES_SLIDE5":"Then it's your opponent's turn.",
 "CLOSE":"Close"
 });
